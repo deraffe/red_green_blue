363c33905a165ddc9ffb64dec8c6715f83316bfe
Sýlvan Heuser <sylvan.heuser@gmx.net>
2019-03-12 17:08:53 +0100

    Add coloring to test script

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 while true; do
   echo
-  ( (echo red; echo green 1>&2) | echo blue ) 2>&1 | tr "\n" " "
+  ( (echo "\e[30;41mred\e[0m"; echo "\e[30;42mgreen\e[0m" 1>&2) | echo "\e[30;44mblue\e[0m" ) 2>&1 | tr "\n" " "
   sleep 0.01s
 done
